89c3ba404bccc6cfec9d0d24f70205451b318526
Denys Pavlyuk <densf2@gmail.com>
2022-05-29 16:02:09 +0300

    added_scenarios_for_check_post_requests

--- test/pet.store.test.ts
+++ test/pet.store.test.ts
@@ -9,9 +9,11 @@ import chaiResponseValidator from 'chai-openapi-response-validator'
 const pathToSpec = path.resolve('3_0/petstore.yaml')
 chai.use(chaiResponseValidator(pathToSpec))
 
-describe('PET Store', () => {
+describe('PET Store', function () {
     let petId = Number
     let petName = 'jack'
+    let petStatusAvailable = 'available'
+    let petStatusSold = 'sold'
     it('adding new pet to the store',async () => {
         const response = await axios.post(
             'https://petstore.swagger.io/v2/pet',
@@ -31,7 +33,7 @@ describe('PET Store', () => {
                         'name': 'best dog'
                     }
                 ],
-                'status': 'available'
+                'status': petStatusAvailable
             },
             {
                 headers: {
@@ -43,14 +45,38 @@ describe('PET Store', () => {
         petId = response.data.id
     })
 
-    it('validate response axios & chai-response',async () => {
-        const res = await axios.get(`http://petstore.swagger.io/v2/pet/${petId}`,
+    it('get by pet id & validate response axios & chai-response',async function () {
+        const res = await axios.get(`http://petstore.swagger.io/v2/pet/8089`,
         {headers: {'accept': 'application/json'}})
 
         expect(res.status).to.equal(200)
         expect(res).to.satisfyApiSpec
         let petNameFromResponse = String(jsonpath.query(res.data, "$.name"))
         expect(petNameFromResponse).to.equal(petName)
+    })
+
+    it('update pet status', async function () {
+        const response = await axios.post(
+            'https://petstore.swagger.io/v2/pet/8089',
+            new URLSearchParams({
+                'status': petStatusSold
+            }),
+            {
+                headers: {
+                    'accept': 'application/json'
+                }
+            }
+        )
+        expect(response.status).to.equal(200)
+    })
+
+    it('get by pet id after change status', async function () {
+        const res = await axios.get(`http://petstore.swagger.io/v2/pet/8089`,
+        {headers: {'accept': 'application/json'}})
+
+        expect(res.status).to.equal(200)
+        let petStatusFromResponse = String(jsonpath.query(res.data, '$.status'))
+        expect(petStatusFromResponse).to.equal(petStatusSold)
     })
 
 })
