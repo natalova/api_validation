025dc5d5f1d4559a57c9de25115b8e3f6ae4b39c
natalova <48879751+natalova@users.noreply.github.com>
2023-08-27 13:58:43 +0300

    Add files via upload

---
+++ nataly_home_task.js
@@ -0,0 +1,18 @@
+//зробити змінну з значенням "We are good"
+let NewOne = 'We are good'
+
+//створити 2 зміні зі значеннями ''Hello" і "World"
+let NewTwo = 'Hello'
+let NewThree = 'World'
+
+//створити команду для виведення в консоль цих слів разом
+console.log (NewTwo , NewThree)
+
+//чи вірно оголошена перевірка типу даних і якщо ні - то змінити на вірний варіан
+let myVariable = 'value'
+console.log (typeof myVariable)
+
+//чи вірно оголошений код для отримання значення іншої змінної
+let name = 'value2'
+let NewFour = `hello ${name}`
+console.log (NewFour)
